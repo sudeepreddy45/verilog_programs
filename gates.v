c9f210f58e429100dd861f701dc93cb9bb9148a6
Sudeep Reddy Pedaballe <sudeepreddy_pedaballe@srmap.edu.in>
2025-08-10 15:54:48 +0530

    Update gates.v

--- gates.v
+++ gates.v
@@ -1,3 +1,4 @@
+dataflow-->
 module gates(
     input a,
     input b,
@@ -11,6 +12,24 @@ module gates(
     assign y[5] = a;           // BUFFER A
     assign y[6] = a ^ b;       // XOR
     assign y[7] = ~(a ^ b);    // XNOR
+endmodule
+
+behavioral-->
+module gates(
+    input  a,
+    input  b,
+    output reg [7:0] y
+);
+    always @(*) begin
+        y[0] = a & b;       // AND
+        y[1] = a | b;       // OR
+        y[2] = ~(a & b);    // NAND
+        y[3] = ~(a | b);    // NOR
+        y[4] = ~a;          // NOT A
+        y[5] = a;           // BUFFER A
+        y[6] = a ^ b;       // XOR
+        y[7] = ~(a ^ b);    // XNOR
+    end
 endmodule
 
 
